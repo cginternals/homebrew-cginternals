379d9f4e1440ccffb9b308d6ee4267bd7c05c2ef
Philipp Otto <philipp.otto2@student.hpi.de>
2016-04-04 09:56:26 +0200

    add cpplocate recipe

---
+++ cpplocate.rb
@@ -0,0 +1,28 @@
+class Cpplocate < Formula
+  desc "C++ library that for applications to locate themselves, their data assets, as well as dependend modules."
+  homepage "https://github.com/cginternals/cpplocate"
+  sha256 ""
+
+  head "https://github.com/cginternals/cpplocate.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "cmake", "--build", ".", "--target", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <cpplocate/cpplocate.h>
+      int main(void)
+      {
+        const cpplocate::ModuleInfo moduleInfo = cpplocate::findModule("cpplocate");
+      }
+      EOS
+    system ENV.cxx, "-o", "test", "test.cpp", "-std=c++11", "-stdlib=libc++",
+                    "-I#{include}/cpplocate", "-I#{lib}/cpplocate",
+                    "-lcpplocate", *ENV.cflags.to_s.split
+    system "./test"
+  end
+end
