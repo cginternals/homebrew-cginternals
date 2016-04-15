c2b3c3f7ab827d9c933a4d8b74acc64b982909da
Philipp Otto <philipp.otto2@student.hpi.de>
2016-04-15 14:13:10 +0200

    Add missing cmake dependency

--- libzeug.rb
+++ libzeug.rb
@@ -4,13 +4,13 @@ class Libzeug < Formula
   url "https://github.com/cginternals/libzeug/archive/v0.6.0.tar.gz"
   sha256 "af3ca78cf01423dfbba80405ea48f81b7b28c7b79be8dc44b7d6f432d837bc3e"
 
+  depends_on "cmake" => :build
+
   needs :cxx11
 
   def install
-    args = []
-
     ENV.cxx11
-    system "cmake", ".", *args, *std_cmake_args
+    system "cmake", ".", *std_cmake_args
     system "cmake", "--build", ".", "--target", "install"
   end
 end
