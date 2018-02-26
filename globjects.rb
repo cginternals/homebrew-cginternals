204683e84c4c35c69e075a3838e64a4e85ed4d85
Willy Scheibel <willyscheibel@gmx.de>
2018-02-26 17:01:58 +0100

    Use versioned URL instead of master

--- globjects.rb
+++ globjects.rb
@@ -1,8 +1,8 @@
 class Globjects < Formula
   desc "C++ library strictly wrapping OpenGL objects"
   homepage "https://github.com/cginternals/globjects"
-  url "https://github.com/cginternals/globjects/archive/master.tar.gz"
-  sha256 "1730449be436ccd55080c7427f1ee82dd4adb4e333d8d43a0851bf88e74ba99d"
+  url "https://github.com/cginternals/globjects/archive/v2.0.tar.gz"
+  sha256 "c387b503d9eebfa0276c4978fc1767d294f818e1f04546c057b7b72be7eb1794"
   head "https://github.com/cginternals/globjects.git"
 
   depends_on "cmake" => :build
